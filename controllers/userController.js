f166af39c50fc74493d20701e32c01a2bed20780
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-04-24 00:08:55 -0400

    userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,8 +9,7 @@ module.exports = {
     },
     //get user by ID
     getUserByID(req, res) { 
-        User
-            .findOne({_id: req.params.userId})
+        User.findOne({_id: req.params.userId})
             .populate({path: 'thought', select:'-__v' })
             .populate({path: 'friends', select:'-__v' })
             .populate('-__v')
@@ -21,17 +20,62 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     //create user
-    // createUser(req, res) {
-    //     User.findOneAndUpdate({_id})
-    // }
+    createUser({body}, res) {
+        User.create(body)
+            .then(data => res.json(data))
+            .catch(err => res.status(400).json(err));
+
+    },
+    //updates user by ID
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+        .then((user) => {
+            !user ? res.status(400).json({message: 'user not found'}) :
+            res.json(user)
+        })
+        .catch(err => res.status(400).json(err))
+    },
     //deletes user by ID
     deleteUser(req, res) {
-        User
-            .findOneAndDelete({_id: req.params.userId})
+        User.findOneAndDelete({_id: req.params.userId})
             .then((user) => {
                 !user ? res.status(404).json({message: 'No user found'}) :
                 res.json(user);
             })
             .catch(err = res.status(400).json(err));
+    },
+    //add friend
+    addFriend(req, res) {
+        User.findOneAndUpdate (
+            {_id: req.params.userId},
+            {$push: {friends: req.params.friendId}},
+            {runValidators: true, new: true}
+        )
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(data => {
+            !data ? res.status(404).json({message: 'No user found'}) :
+            res.json(DataTransfer)
+        })
+        .catch(err => res.json(err));
+    },
+    //delete friend
+    deleteFriend(req, res) {
+        User.findOneAndUpdate (
+            {_id: req.params.userId},
+            {$pull: {friends: req.params.friendId}},
+            {runValidators: true, new: true}
+        )
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(data => {
+            !data ? res.status(404).json({message: 'No user found'}) :
+            res.json(DataTransfer)
+        })
+        .catch(err => res.json(err));
     }
 }
