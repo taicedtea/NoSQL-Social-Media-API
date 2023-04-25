2d315e27763ef4965dc30ce1468a1f4d3ebb81ce
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-04-25 12:25:29 -0400

    Added some thought controller functionality

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,13 +8,20 @@ module.exports = {
             .catch((err) => res.status(500).json(err))
     },
     //gets thought by ID
-    getThoughtById(req, res) {
-        Thought.findOne({_id: req.params.thoughtId})
-        .then((thought) => {
-            if (!thought) {
-                res.status(400).json('No thought found.');
-                return;
-            } res.json(thought)
+    getThoughtById({params}, res) {
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((data) => {
+                !data ? res.status(404).json({ message: "No though with this id!" }) :
+                res.json(data);
+            })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
         });
     },
     //creates thought to user by ID
@@ -24,28 +31,17 @@ module.exports = {
                 return User.findOneAndUpdate(
                     {_id: body.userId},
                     {$addToSet: {thought: thought._id}},
+                    {new: true}
                 );
             })
-            .then(data => {
-                !data ? res.status(404).json({message: 'No user found'}) :
-                res.json(data)
+            .then(user => {
+                !user ? res.status(404).json({message: 'Thought created without user ID'}) :
+                res.json('Thought created')
             })
             .catch(err => res.status(500).json(err));
     },
     //updates thoughts via ID
-    updateThought(req,res) {
-        Thought
-        .findByIdAndUpdate(
-            {_id: req.params.thoughtId},
-            {$set: req.body},
-            {new: true, runValidators: true})
-        .populate({path: 'reactions', select: '-__v'})
-        .select('-___v')
-        .then(thought => {
-            !thought ? res.status(404).json({message: 'No thoughts with this ID found'}) :
-            res.json(thought);
-        })
-        .catch(err => res.status(400).json(err));
+    updateThought (req, res) {
     },
     //delete thought by ID
     deleteThought(req, res) {
